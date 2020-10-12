7f9938982bc6ccacf4926de11453597624c390f0
doron <doron.miller@gmail.com>
2020-10-12 17:14:46 +0300

    12.4

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.456a66d030644f812e29e8aff9811aa6.js"
+  "./precache-manifest.13f551483bb527cf28cc7dbc6a219778.js"
 );
 
 self.addEventListener('message', (event) => {
